129ef78a6343be704c4feca9276c237959ebb991
Jin Drew IT <110651814+qJinDrew@users.noreply.github.com>
2022-11-20 10:39:08 +0300

    Add completed test working.

--- webserv/main/views.py
+++ webserv/main/views.py
@@ -5,3 +5,6 @@ def index(request):
     msg = request.GET['message']
     name = request.GET['name']
     return render(request, 'main/index.html', {'message': msg, 'name': name})
+
+def short(request):
+    return render(request, 'main/short.html')
--- webserv/webserv/settings.py
+++ webserv/webserv/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'webserv.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [''],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -117,7 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
